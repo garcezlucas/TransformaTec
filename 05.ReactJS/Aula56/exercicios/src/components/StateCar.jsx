026f9cafab5b6ed48221e799e76469515c3dde41
Badegoots <garcezlucas.silva@gmail.com>
2022-05-31 07:53:01 -0300

    commit Aula 56

--- 05.ReactJS/Aula56/exercicios/src/components/StateCar.jsx
+++ 05.ReactJS/Aula56/exercicios/src/components/StateCar.jsx
@@ -4,33 +4,29 @@ export default class StateCar extends React.Component {
 
     constructor(props) {
         super(props);
-        this.modelo = "fusca"
+        this.modelo = "Fusca"
         this.state = {
             ligado: false,
             velocidade: 0
         }
 
     }
-
     ligarDesligar(){
         this.setState({ligado: !this.state.ligado})
     }
-
     acelerar(){
-        let vel = this.state.velocidade + 0.5
+        let vel = this.state.velocidade + 5
         this.setState({velocidade: vel})
     }
-
     frear(){
-        let vel = this.state.velocidade - 0.5
+        let vel = this.state.velocidade - 2
         this.setState({velocidade: vel})
     }
-
     parar(){
         //this.state.velocidade = 0
-        this.setState({velocidade: 0})
+        console.log("V: ", this.state.velocidade)
+        //this.setState({velocidade: 0})
     }
-
     render(){
         return(
             <div>
@@ -40,22 +36,16 @@ export default class StateCar extends React.Component {
                 <p>Ligado: {this.state.ligado ? "Sim" : "Não"}</p>
 
                 <p>Velocidade: {this.state.velocidade}</p>
+
+                <button onClick={() => this.ligarDesligar()}>Girar Chave</button>
+
+                <button onClick={() => this.acelerar()}>Acelerar</button>
+
+                <button onClick={() => this.frear()}>Frear</button>
                 
-                <button type="button" onClick={() => this.ligarDesligar()}>Ligar/Desligar</button>
-                
-                <button type="button" onClick={() => this.acelerar()}>Acelerar</button>
-
-                <button type="button" onClick={() => this.frear()}>Frear</button>
-
-                <button type="button" onClick={() => this.parar()}>Parar</button>
-
-                
-
+                <button onClick={() => this.parar()}>Parar</button>
             </div>
         )
     }
+
 }
-
-
-
-
--- 05.ReactJS/Aula56/exercicios/src/components/StatePerson.jsx
+++ 05.ReactJS/Aula56/exercicios/src/components/StatePerson.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
 
-export default class StatePerson extends React.Component{
+export default class StatePerson extends React.Component {
 
-    constructor(props){
-    
+    constructor(props) {
         super(props);
+
         this.state = {
-            nome: "Lucas",
-            profissao: "Oceanografo",
-            idade: 27,
+            nome:"Jose",
+            profissao:"Marceneiro",
+            idade: 40,
         }
 
         this.atualizarPessoa = this.atualizarPessoa.bind(this)
-    
     }
 
     atualizarPessoa(){
         this.setState({
-            nome: this.props.nome,
-            profissao: this.props.profissao,
-            idade: this.props.idade,
+            nome:this.props.nome,
+            profissao:this.props.profissao,
+            idade:this.props.idade
         })
     }
 
     render(){
-        return(
+        return (
             <div>
-
                 <p>Nome: {this.state.nome}</p>
                 <p>Profissão: {this.state.profissao}</p>
                 <p>Idade: {this.state.idade}</p>
-
-                <button type="button" onClick={() => this.atualizarPessoa()}>Atualizar Pessoa</button>
-
+                <button onClick={this.atualizarPessoa}>Atualizar</button>
             </div>
         )
     }
+
 }
