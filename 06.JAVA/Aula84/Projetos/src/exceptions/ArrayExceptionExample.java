fce6e184c672df542b588ae25f78fe81c82bdade
Badegoots <garcezlucas.silva@gmail.com>
2022-07-18 22:06:56 -0300

    commit Aula 84

---
+++ 06.JAVA/Aula84/Projetos/src/exceptions/ArrayExceptionExample.java
@@ -0,0 +1,33 @@
+package exceptions;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayExceptionExample {
+    
+    static void getName(List<String> names) throws IndexOutOfBoundsException{
+
+        Integer i = 3;
+
+
+        if(names.size() <= i){
+            throw new IndexOutOfBoundsException("Index fora do array");
+        }
+
+        String name = names.get(i);
+
+        System.out.println(name);
+
+    }
+
+    public static void main(String[] args) {
+        
+        List<String> names = new ArrayList<String>();
+
+        names.add("Lucas"); //0
+        names.add("Luca");   //1
+        names.add("Lu");    //2
+
+        getName(names);
+
+    }
+}
