3828e3240c160437ec3e3e419c9a9d28c57ccbd4
Badegoots <garcezlucas.silva@gmail.com>
2022-08-26 18:50:51 -0300

    commit Aula 109

---
+++ 08.SpringBoot/Aula109/exemplo/src/main/java/com/aula109/exemplo/Application.java
@@ -0,0 +1,38 @@
+package com.aula109.exemplo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    StudentRepository repo;
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args){
+
+        repo.save(new Student("James", 17));
+        repo.save(new Student("John", 16));
+        repo.save(new Student("Mark", 15));
+        repo.save(new Student("Carl", 18));
+        repo.save(new Student("Bob", 19));
+
+        System.out.println("\n--> Lista de aluno:\n");
+
+        for (Student student : repo.findAll()) {
+            System.out.println("\n--> Dados do aluno:\n");
+            System.out.println(student.toString());
+        }
+
+        Student student = repo.findByName("Mark");
+        System.out.println("\n--> Procurando Mark:\n");
+        System.out.println(student.toString());
+    }
+    
+}
