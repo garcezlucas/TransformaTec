4e66c7e06f925cb5fb54fad894c91afb9b54df21
Badegoots <garcezlucas.silva@gmail.com>
2022-09-06 21:55:24 -0300

    commit Organizacional

--- 09.FullStack.React.Spring/Aula127/project/src/main/java/com/aula126/project/controllers/GroupController.java
+++ 09.FullStack.React.Spring/Aula127/project/src/main/java/com/aula126/project/controllers/GroupController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class GroupController {
 
     @Autowired
