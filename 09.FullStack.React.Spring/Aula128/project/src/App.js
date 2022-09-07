6880fd3a57b2076f223ed9b90e146c25a7b31dcf
Badegoots <garcezlucas.silva@gmail.com>
2022-09-06 21:56:06 -0300

    commit Aula 128

---
+++ 09.FullStack.React.Spring/Aula128/project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SingUp from './components/SignUp'
+import ContactRegister from './components/ContactRegister'
+
+function App() {
+  return (
+    <div>
+      <SingUp />
+      <ContactRegister />
+    </div>
+  );
+}
+
+export default App;
