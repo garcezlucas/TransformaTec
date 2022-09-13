14120f347e56f6b4da9d45a3087aabffa7552640
Badegoots <garcezlucas.silva@gmail.com>
2022-09-13 15:03:22 -0300

    commit Aula 131

---
+++ 09.FullStack.React.Spring/Aula131/project/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import SingUp from './components/SignUp'
+import ContactRegister from './components/ContactRegister'
+import SignIn from './components/SignIn';
+import GroupRegister from './components/GroupRegister';
+import PhoneRegister from './components/PhoneRegister';
+import ContactList from './components/ContactList';
+import PhoneList from './components/PhoneList';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      {/* <SingUp />
+      <br />
+      <SignIn />
+      <br />
+      <ContactRegister />
+      <br />
+      <GroupRegister />
+      <br />
+      <PhoneRegister />
+      <br />
+      <ContactList />
+      <br />
+
+      <PhoneList />
+      <br /> */}
+      <Home />
+      <br />
+    </div>
+  );
+}
+
+export default App;
